7b2c3e16d527b65d2026af5caba04a146cef73b0
Anthony O’Leary <anthony.oleary@finddx.org>
2016-01-05 09:53:52 +0000

    added anomalies filter for missing sampleID and start_time, also changed alert percolator to include category in the index

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20160115131955) do
-
 
   create_table "alert_histories", force: :cascade do |t|
     t.boolean  "read",                                  default: false
@@ -55,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160115131955) do
     t.integer  "channel_type",          limit: 4,     default: 0
     t.integer  "aggregation_frequency", limit: 4,     default: 0
     t.integer  "sms_limit",             limit: 4,     default: 0
+    t.integer  "anomalie_type",         limit: 4,     default: 0
   end
 
   add_index "alerts", ["user_id"], name: "index_alerts_on_user_id", using: :btree
