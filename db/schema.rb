11b8314c29a4a0e2e9bc47461b43480e5efe4c56
Anthony O’Leary <anthony.oleary@finddx.org>
2016-01-19 06:17:56 +0000

    added changes to edit new alert for external users

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115131955) do
-
+ActiveRecord::Schema.define(version: 20160115135130) do
 
   create_table "alert_histories", force: :cascade do |t|
     t.boolean  "read",                                  default: false
