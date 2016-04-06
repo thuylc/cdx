e2a4d047f67b98a24e279750b12cb0ea134d5861
Stewart Campbell <stewartcuk@gmail.com>
2016-04-06 13:40:24 +0100

    fixing site reports

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322190646) do
+ActiveRecord::Schema.define(version: 20160330100508) do
 
   create_table "alert_condition_results", force: :cascade do |t|
     t.string  "result",   limit: 255
@@ -183,8 +183,8 @@ ActiveRecord::Schema.define(version: 20160322190646) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "institution_id",                  limit: 4
-    t.boolean  "supports_activation"
     t.datetime "published_at"
+    t.boolean  "supports_activation"
     t.string   "support_url",                     limit: 255
     t.string   "picture_file_name",               limit: 255
     t.string   "picture_content_type",            limit: 255
